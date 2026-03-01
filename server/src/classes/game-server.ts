b926e13c63c31007598a206f58c275d8456d396e
Marlo Dela Torre <marlodelatorre11@gmail.com>
2026-03-02 01:28:53 +0800

    fix: remove port from ws instance

--- server/src/classes/game-server.ts
+++ server/src/classes/game-server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import http from 'http';
-import { WebSocketServer, type ServerOptions } from 'ws';
+import { WebSocketServer } from 'ws';
 
 import { WS_PORT } from '@root/const/config';
 import { GAME_EVENT } from '@root/const/game';
@@ -11,6 +11,7 @@ import { WSWebSocket } from './types';
 const app = express();
 app.use(express.json());
 const server = http.createServer(app);
+const wss = new WebSocketServer({ server, autoPong: true });
 
 const GAMES = {
   MATH: 'math',
@@ -29,14 +30,6 @@ export class GameServer {
   }
 
   start() {
-    const config: ServerOptions = { server, autoPong: true };
-
-    if (process.env.NODE_ENV === 'production') {
-      config['port'] = WS_PORT;
-    }
-
-    const wss = new WebSocketServer(config);
-
     wss.on('connection', (ws) => {
       const player = new Player(ws);
       this.clients.set(player.id, player.ws);
